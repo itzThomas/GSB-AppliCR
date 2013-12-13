ec5b233f542aa0ecd0fad185f81a12f7539fbb17
Thomas <thomasds.p@hotmail.fr>
2013-12-13 09:13:52 +0100

    Base codeIgniter Propre

---
+++ GSB-AppliCR/application/controllers/c_connexion.php
@@ -0,0 +1,95 @@
+﻿<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class C_connexion extends CI_Controller {
+	
+    public function index()
+    
+    { 
+    	
+    	$data['titre'] = 'Connexion Site';
+    	$data['access'] = $this->config->item('dossier_access');
+    	
+    	
+    	//chargement du modele et de la page de connxion 	
+    	$this->load->model('m_connexion'); 
+    	$this->load->view('v_header',$data);
+  		$this->load->view('v_connexion');
+    	
+    }
+
+    public function connexion(){
+    	$this->load->library('session');
+    	$data['titre'] = 'Sommaire';
+    	$data['access'] = $this->config->item('dossier_access');
+    	 
+    
+    	// Chargement de la bibliothèque
+    	$this->load->model('m_connexion');
+    	$this->load->library('form_validation');
+    	 
+    	// Règle des champs
+    	$this->form_validation->set_rules('login','"login"','required | min_length[5]');
+    	$this->form_validation->set_rules('mdp','"mdp"','required | min_length[5]');
+   
+    	// verification 
+    	if ($this->form_validation->run() == TRUE)
+    	{	 		
+    		$login = mysql_real_escape_string($this->input->post('login'));
+    		$mdp = mysql_real_escape_string($this->input->post('mdp'));
+    		
+    		$resultat = $this->m_connexion->connexion($login,$mdp);
+    		
+    		if ($resultat == 1){
+    			
+    			$info = $this->m_connexion->recupInfo($login);
+    			 			
+    			$nom = $info[0]['nom'];
+    			$id = $info[0]['id'];
+    			$prenom = $info[0]['prenom'];
+    			
+    			$this->session->set_userdata('login',$login);
+    			
+    			$this->session->set_userdata('mdp',$mdp);
+    			$this->session->set_userdata('nom',$nom);
+    			$this->session->set_userdata('id',$id);
+    			$this->session->set_userdata('prenom',$prenom);
+    			
+    			
+    			
+ 				$data['nom'] = $nom;
+ 				$data['id'] = $id;
+ 				
+ 				
+    			$this->load->view('v_header',$data);
+    			$this->load->view('v_sommaire',$data);
+    		
+    			
+    			//die("vrai");
+    			
+    	
+    			
+    		}
+    		else{
+    			//$this->load->view('v_header',$data);
+    			$this->load->model('m_connexion');
+    			
+    			$this->load->view('v_erreurs',$data);
+    			//die("faux");
+    		}
+    		
+    	}
+    	else{
+    		
+    		$this->load->view('v_header',$config);
+    		$this->load->view('v_erreurs');
+    		//die("erreur");
+    	}
+}
+
+
+public function deconnexion() {
+	$this->session->sess_destroy();
+	$this->index();
+}
+}
+?>
