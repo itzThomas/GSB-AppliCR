b36d861077a9ad6ee283591fe470446c119e3dca
itzThomas <thomasds.p@hotmail.fr>
2013-12-17 11:04:52 +0100

    Liste déroulante sur la page Médicament

--- application/models/m_medicament.php
+++ application/models/m_medicament.php
@@ -7,9 +7,13 @@ class M_medicament extends CI_Model{
 	function recupInfo(){
 		$reponse = ("SELECT * FROM medicament ");
 		$query = $this->db->query($reponse);
-
+		return $query->result();		
+	}
+	
+	function recupNom(){
+		$reponse = ("SELECT MED_NOMCOMMERCIAL FROM medicament ");
+		$query = $this->db->query($reponse);
 		return $query->result_array();
-		
 	}
 	
 }
--- application/views/v_medicament.php
+++ application/views/v_medicament.php
@@ -2,30 +2,47 @@
 <div id="page">
 	<div id="entete">
 
-		
+
 		<?php $image_properties = array(
-		'src' => ''.$access.'/assets/img/logo.jpg',
-		'width' => '150',
-		'height' => '150',
-		'title' => 'GSB',
-		'id' => 'logoGSB',
+				'src' => ''.$access.'/assets/img/logo.jpg',
+				'width' => '150',
+				'height' => '150',
+				'title' => 'GSB',
+				'id' => 'logoGSB',
 
 	);?>
-	<?php echo img($image_properties); ?>
+		<?php echo img($image_properties); ?>
 
 
 	</div>
-	<?php echo form_open('c_medicament/affichage');
-		echo form_error();?>
-
-
-	<div id="contenu">
+		<div id="contenu">
 		<h1>Medicaments :</h1>
-  
-		<p> 
-			<label for="nom">Depot légal :</label> 
+	<?php $this->load->helper('form');?>
+	<?php $this->load->helper('html');?>
 
-			<?php echo form_input(""); ?>
+<?php 
+	$tab=array();
+	 foreach ($affiche as $item) {?>
+	<?php
+	$item-> MED_NOMCOMMERCIAL;
+	$tab[$item]?>
+	 
+	<?php }?>
+ 	<br/>
+ 	<?php 	echo form_dropdown('nomMedoc', $tab);
+	echo "<pre>".print_r($affiche, true)."</pre>";
+	
+	?>
+ 
+
+
+
+
+
+		<p>
+			<label for="nom">Depot légal :</label>
+
+			<?php echo form_input(); ?>
 			</br>
 		</p>
 		<p>
@@ -33,11 +50,11 @@
 
 			<?php echo form_input(""); ?>
 			</br>
-			
+
 		</p>
 		<p>
-			<label for="nom">famille :</label>
-  
+			<label for="nom">Famille :</label>
+
 			<?php echo form_input(""); ?>
 			</br>
 		</p>
@@ -47,13 +64,13 @@
 			<?php echo form_input(""); ?>
 			</br>
 		</p>
-			<p>
+		<p>
 			<label for="nom">Effet :</label>
 
 			<?php echo form_input(""); ?>
 			</br>
 		</p>
-			<p>
+		<p>
 			<label for="nom">Contre Indic :</label>
 
 			<?php echo form_input(""); ?>
@@ -65,3 +82,6 @@
 			<?php echo form_input(""); ?>
 			</br>
 		</p>
+
+	</div>
+</div>
