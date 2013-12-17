cdaea7a336830c6cbb5cc9472e76f47ac7caf0ec
Landrydevy <landry.devy@gmail.com>
2013-12-17 09:04:46 +0100

    !?

--- GSB-AppliCR/application/views/v_erreurs.php
+++ GSB-AppliCR/application/views/v_erreurs.php
@@ -1,28 +1,28 @@
 
-
-<?php   
+    
+ <?php   
 echo link_tag (''.$access.'/assets/css/styles.css');
 
 
-$image_properties = array(
-		'src' => ''.$access.'/assets/img/logo.jpg',
-		'width' => '150',
-		'height' => '150',
-		'title' => 'GSB',
-		'id' => 'logoGSB',
-
+ $image_properties = array(
+ 			'src' => ''.$access.'/assets/img/logo.jpg',
+ 			'width' => '150',
+ 			'height' => '150',
+ 			'title' => 'GSB',
+			'id' => 'logoGSB',
+		
 	);?>
-<div id="page">
-	<div id="entete">
-		<?php echo img($image_properties); ?>
-		<h1>Suivi du remboursement des frais</h1>
+ 	<div id="page">
+<div id="entete">
+<?php echo img($image_properties); ?>
+<h1>Suivi du remboursement des frais</h1>
 
 
-	</div>
+</div>
+ 	 
+ 	
+<div id="contenu">
 
-
-	<div id="contenu">
-	
      <h2  style="color: red">
      	<ul>
 	     	<li>Identification utilisateur</li>
@@ -31,30 +31,26 @@ $image_properties = array(
 	</h2>
  
 
-
-
-
-		<?php echo form_open('c_connexion/connexion');
-		echo form_error();
-		?>
-
-		<p>
+ 	<?php echo form_open('c_connexion/connexion');
+ 		  echo form_error();
+    ?>
+    
+			<p>
 			<?php echo validation_errors(); echo form_open('c_connexion/connexion');?>
-
-			<label for="nom">Login*</label>
-
-			<?php echo form_input("login"," Votre login"); ?>
-			</br>
-		</p>
-		<p>
-			<label for="mdp">Mot de passe*</label>
-
-			<?php echo form_password("mdp")?>
-			</br>
-		</p>
-		<?php echo form_submit("connexion","connexion"); 
-		echo  form_reset("effacer","Effacer");
+			
+       <label for="nom">Login*</label>
+       
+       <?php echo form_input("login"," Votre login"); ?></br>
+      </p>
+			<p>
+				<label for="mdp">Mot de passe*</label>
+			    
+			  <?php echo form_password("mdp")?></br>
+      </p>
+        <?php echo form_submit("connexion","connexion"); 
+		echo  form_reset("effacer","Effacer"); 
 		echo form_close();
-		?>
+	?>
 
-	</div>
+</div> 
+			
